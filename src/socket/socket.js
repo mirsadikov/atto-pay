14d8fc594f5a45841d7471c9e871117d0280e282
Mirabror Sodikov <mirabror9545@gmail.com>
2023-11-15 16:22:09 +0500

    fix socket cors

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,11 @@ const { Server } = require('socket.io');
 const qrLoginRequest = require('./events/qrLogin');
 const errorHandler = require('./errorHandler');
 
-const io = new Server();
+const io = new Server({
+  cors: {
+    origin: '*',
+  },
+})
 
 io.on('connection', (socket) => {
   socket.on('qr_login_request', errorHandler(socket, qrLoginRequest));
